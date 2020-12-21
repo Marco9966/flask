ad81dd9682e16e82cfee3d6f0e50305e90d7e7be
Marco9966 <marco.antonio.mendel@hotmail.com>
2020-12-21 10:13:56 -0300

    Adicionado um template base para todos os HTMLs, para  maior agilidade e praticidade na hora de escrever novas  paginas.  Adicionado uma tela de login, porem ainda não armazena  nenhuma informação durante a sessão.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   jogoteca.py
    	new file:   static/bootstrap.css
    	modified:   templates/lista.html
    	new file:   templates/login.html
    	new file:   templates/novo.html
    	new file:   templates/template.html

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -8,14 +8,37 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
-    jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
-    jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+lista = [jogo1, jogo2, jogo3]
 
-    lista = [jogo1, jogo2, jogo3]
-
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.run(host='0.0.0.0', port='8080')
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', titulo='Login')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+@app.route('/autenticar', methods=['post',])
+def autenticar():
+    if 'mestra' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+app.run(host='0.0.0.0', port='8080', debug=True)
